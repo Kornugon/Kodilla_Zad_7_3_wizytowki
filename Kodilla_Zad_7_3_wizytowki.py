5e862089a8899887fc1dcf28648c0b05d9abeede
Piotr Loba <piotr.loba22@gmail.com>
2020-12-03 00:19:05 +0100

    correct function names and added block if name...

--- Kodilla_Zad_7_3_wizytowki.py
+++ Kodilla_Zad_7_3_wizytowki.py
@@ -16,8 +16,11 @@ Wykorzystaj bibliotekę faker do generowania danych.
 from faker import Faker
 fake = Faker()
 
+cont_1_type = "Priv"
+cont_2_type = "Company"
+
 class BaseContact:
-    def __init__(self, first_name, last_name, tel_priv, email):
+    def __init__(self, first_name: str, last_name: str, tel_priv: str, email: str):
        self.first_name = first_name
        self.last_name = last_name
        self.tel_priv = tel_priv
@@ -30,24 +33,17 @@ class BaseContact:
     #def __repr__(self):
     #  return f'{self.first_name} {self.last_name} {self.email}'
 
+
     def contact(self):
         return f"Wybieram numer {self.tel_priv} i dzwonię do {self.first_name} {self.last_name}"
 
     @property
-    def get_name_len(self):
+    def name_len(self):
         return len(self.first_name) + len(self.last_name)
-    
-    #Blok BusinessContact ma tą funkcję i @property przez dziedziczenie więc nie trzeba tego powielać
-    @get_name_len.setter
-    def get_name_len(self, name):
-        first_name, last_name = name.split(" ")
-        self.first_name = first_name
-        self.last_name = last_name
-
 
 
 class BusinessContact(BaseContact):
-    def __init__(self, position, company_name, tel_comp, *args, **kwargs):
+    def __init__(self, position: str, company_name: str, tel_comp: str, *args, **kwargs):
        super().__init__(*args, **kwargs)
        self.position = position
        self.company_name = company_name
@@ -61,58 +57,58 @@ class BusinessContact(BaseContact):
 
 
 
-def create_contacts(contact_obj, amount):
+def create_contacts(contact_obj: str, amount: int):
     """
     Faker to generate false personal data.
     Instet "Priv" in first position for private data,
     Insert "Company" in first position for corporate data.
     Second position is for amount of data.
     """
-    if contact_obj == "Priv":
+    if contact_obj == cont_1_type:
         for i in range(amount):
             cont = BaseContact(first_name=fake.first_name(), last_name=fake.last_name(), tel_priv=fake.phone_number(), email=fake.email())
             print(f"Wizytówka {i+1} prywatna: {cont}")
-    elif contact_obj == "Company":
+    elif contact_obj == cont_2_type:
         for i in range(amount):
             cont = BusinessContact(first_name=fake.first_name(), last_name=fake.last_name(), tel_priv=fake.phone_number(), email=fake.email(), position=fake.job(), company_name=fake.company(), tel_comp=fake.phone_number())
             print(f"Wizytówka {i+1} firmowa: {cont}")
 
 
 
-#wywołanie 10 fałszywych kontaktów
-create_contacts("Priv", 10)
-create_contacts("Company", 10)
+
+
+if __name__ == '__main__':
+
+    #wywołanie 10 fałszywych kontaktów
+    create_contacts(cont_1_type, 10)
+    create_contacts(cont_2_type, 10)
 
 
 
-# printy sprawdzajace dla Priv
-con_1 = BaseContact(
-    first_name="Jan", 
-    last_name="Kowalski", 
-    tel_priv="+48 555 666 999", 
-    email="Jan.Kowal@Dgmail.com"
-    )
 
-print(con_1.contact())
+    # printy sprawdzajace dla Priv
+    con_1 = BaseContact(
+        first_name="Jan", 
+        last_name="Kowalski", 
+        tel_priv="+48 555 666 999", 
+        email="Jan.Kowal@Dgmail.com"
+        )
 
-print(con_1.get_name_len)
-con_1.get_name_len="Kuba KKK"
-print(con_1.get_name_len)
+    print(con_1.contact())
+    print(con_1.name_len)
 
 
-# printy sprawdzajace dla Company
-con_2 = BusinessContact(
-    first_name="Jan", 
-    last_name="Kowal", 
-    tel_priv="+48 555 666 999", 
-    email="Jan.Kowal@Dgmail.com", 
-    position="Self Employment", 
-    company_name="Private Company", 
-    tel_comp="+48 111 222 666"
-    )
 
-print(con_2.contact())
+    # printy sprawdzajace dla Company
+    con_2 = BusinessContact(
+        first_name="Jan", 
+        last_name="Kowal", 
+        tel_priv="+48 555 666 999", 
+        email="Jan.Kowal@Dgmail.com", 
+        position="Self Employment", 
+        company_name="Private Company", 
+        tel_comp="+48 111 222 666"
+        )
 
-print(con_2.get_name_len)
-con_2.get_name_len="Ludwik XXXVI"
-print(con_2.get_name_len)
+    print(con_2.contact())
+    print(con_2.name_len)
